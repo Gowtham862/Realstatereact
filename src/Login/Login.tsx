e296e695a200d400105f3364aae376932f9a91fb
Gowtham862 <gowthamk712812@gmail.com>
2026-05-19 11:39:43 +0530

    fix in login

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -46,7 +46,7 @@ function Login() {
           <input name="password" type="password" placeholder="Password" />
           <button disabled={loading}>Login</button>
           {error && <span>{error}</span>}
-          <Link to="/register">{"Don't"} you have an account?</Link>
+          <Link to="/register">{"Don't"} you have an account? yes</Link>
         </form>
       </div>
       <div className="imgContainer">
